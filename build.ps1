9d418dbfea3287b35e553fa226b8548023425cc4
neolithos <neolithos@outlook.com>
2017-10-01 15:39:42 +0200

    Chg: Use only one module

--- build.ps1
+++ build.ps1
@@ -1,55 +1,9 @@
+Import-Module ..\kenworth_w900_dashboard\build_scs.psm1
 
-$rootPath = "D:\ATS\peterbilt_389_dashboard";
-$compress = @(
+New-ScsMod -Name peterbilt_389_dashboard_neo -RootPath $PSScriptRoot -Steam $true -Content @(
 	"icon.jpg",
 	"manifest.sii",
-	"mod_description.txt"
+	"mod_description.txt",
+
+	"ui"
 );
-
-$ofs = $rootPath.Length + 1;
-foreach ($fi in Get-ChildItem -Recurse (Join-Path $rootPath .\ui)) {
-	if (Test-Path -PathType Leaf $fi.FullName) {
-		$compress += $fi.FullName.Substring($ofs);
-	}
-}
-
-$targetZip = Join-Path $rootPath .\bin\peterbilt_389_dashboard_neo.zip;
-$targetSteam = Join-Path $rootPath .\bin\steam
-Remove-Item -Recurse $targetZip -ErrorAction SilentlyContinue
-Remove-Item -Recurse $targetSteam -ErrorAction SilentlyContinue
-
-$zip = [System.IO.Compression.ZipFile]::Open($targetZip, [System.IO.Compression.ZipArchiveMode]::Create);
-
-Write-Host "Pack:";
-foreach ($cur in $compress) {
-
-	if ($cur.EndsWith("/")) {
-		Write-Host $cur;
-		$_ = $zip.CreateEntry($cur, [System.IO.Compression.CompressionLevel]::NoCompression);
-	} else {
-		$fullSource = Join-Path $rootPath $cur;
-
-        # zip
-		$entryName = $cur.Replace("\", "/");
-		Write-Host $entryName;
-		$_ = [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $fullSource, $entryName);
-
-        # steam
-        #$fi = New-Object -TypeName System.IO.FileInfo -ArgumentList (Join-Path (Join-Path $targetSteam "universal") $cur);
-        #if (!($fi.Directory.Exists)) {
-        #    $fi.Directory.Create();
-        #}
-        #Copy-Item $fullSource $fi.FullName;
-	}
-}
-
-$zip.Dispose();
-
-Copy-Item $targetZip -Destination "C:\Users\Stein\Documents\American Truck Simulator\mod\peterbilt_389_dashboard_neo.scs"
-
-$fi = New-Object -TypeName System.IO.FileInfo -ArgumentList (Join-Path $targetSteam "universal.zip");
-if (!($fi.Directory.Exists)) {
-    $fi.Directory.Create();
-}
-Copy-Item $targetZip -Destination $fi.FullName;
-Copy-Item .\versions.sii -Destination $targetSteam;
